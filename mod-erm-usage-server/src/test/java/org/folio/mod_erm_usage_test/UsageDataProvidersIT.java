712308da3145cee233eee387df9da78b89ae39d0
Tino Ryll <ryll@ub.uni-leipzig.de>
2018-09-11 13:26:44 +0200

    Update schema of UsageDataProviders and IT

--- mod-erm-usage-server/src/test/java/org/folio/mod_erm_usage_test/UsageDataProvidersIT.java
+++ mod-erm-usage-server/src/test/java/org/folio/mod_erm_usage_test/UsageDataProvidersIT.java
@@ -3,19 +3,10 @@ package org.folio.mod_erm_usage_test;
 import static com.jayway.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-
-import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.http.ContentType;
-import com.jayway.restassured.parsing.Parser;
-import io.vertx.core.Context;
-import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
-import io.vertx.ext.unit.Async;
-import io.vertx.ext.unit.TestContext;
-import io.vertx.ext.unit.junit.Timeout;
-import io.vertx.ext.unit.junit.VertxUnitRunner;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.folio.rest.RestVerticle;
 import org.folio.rest.client.TenantClient;
 import org.folio.rest.jaxrs.model.UdProvidersDataCollection;
@@ -27,6 +18,17 @@ import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.parsing.Parser;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.Timeout;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
 
 @RunWith(VertxUnitRunner.class)
 public class UsageDataProvidersIT {
@@ -35,15 +37,29 @@ public class UsageDataProvidersIT {
   public static final String BASE_URI = "/usage-data-providers";
   private static final String TENANT = "diku";
   private static Vertx vertx;
-  private static Context vertxContext;
   private static int port;
+  private static UsageDataProvider udprovider;
+  private static UsageDataProvider udproviderChanged;
+
   @Rule
   public Timeout timeout = Timeout.seconds(10);
 
   @BeforeClass
   public static void setUp(TestContext context) {
     vertx = Vertx.vertx();
-    vertxContext = vertx.getOrCreateContext();
+
+    // setup sample data
+    try {
+      String udproviderStr =
+          new String(Files.readAllBytes(Paths.get("../ramls/examples/udproviders2.sample")));
+      udprovider = Json.decodeValue(udproviderStr, UsageDataProvider.class);
+      udproviderChanged = Json.decodeValue(udproviderStr, UsageDataProvider.class)
+          .withLabel("CHANGED")
+          .withRequestorMail("CHANGED@ub.uni-leipzig.de");
+    } catch (IOException ex) {
+      context.fail(ex);
+    }
+
     try {
       PostgresClient.setIsEmbedded(true);
       PostgresClient instance = PostgresClient.getInstance(vertx);
@@ -61,9 +77,8 @@ public class UsageDataProvidersIT {
     RestAssured.defaultParser = Parser.JSON;
 
     TenantClient tenantClient = new TenantClient("localhost", port, "diku", "diku");
-    DeploymentOptions options = new DeploymentOptions()
-        .setConfig(new JsonObject().put("http.port", port))
-        .setWorker(true);
+    DeploymentOptions options =
+        new DeploymentOptions().setConfig(new JsonObject().put("http.port", port)).setWorker(true);
 
     vertx.deployVerticle(RestVerticle.class.getName(), options, res -> {
       try {
@@ -87,101 +102,68 @@ public class UsageDataProvidersIT {
 
   @Test
   public void checkThatWeCanAddGetPutAndDeleteUsageDataProviders() {
-    UsageDataProvider usageDataProvider = given()
-        .body("{\n"
-            + " \"id\": \"fc20602a-8ade-4b66-90d0-2853c99affa5\",\n"
-            + " \"label\": \"Nature Sushi\",\n"
-            + " \"vendorId\": \"uuid-123456789\",\n"
-            + " \"platformId\": \"uuid-123456789\",\n"
-            + " \"harvestingStatus\": \"active\",\n"
-            + " \"serviceType\": \"Sushi Lite\",\n"
-            + " \"serviceUrl\": \"http://example.com\",\n"
-            + " \"reportRelease\": 4,\n"
-            + " \"requestedReports\": [\n"
-            + "  \"JR1\"\n"
-            + " ],"
-            + " \"customerId\": \"12345def\",\n"
-            + " \"requestorId\": \"1234abcd\",\n"
-            + " \"apiKey\": \"678iuoi\",\n"
-            + " \"requestorName\": \"Karla Kolumna\",\n"
-            + " \"requestorMail\": \"kolumna@ub.uni-leipzig.de\"\n"
-            + "}")
+    // POST
+    given().body(Json.encode(udprovider))
         .header("X-Okapi-Tenant", TENANT)
         .header("content-type", APPLICATION_JSON)
         .header("accept", APPLICATION_JSON)
         .request()
         .post(BASE_URI)
-        .thenReturn()
-        .as(UsageDataProvider.class);
-    assertThat(usageDataProvider.getLabel()).isEqualTo("Nature Sushi");
-    assertThat(usageDataProvider.getId()).isNotEmpty();
+        .then()
+        .statusCode(201)
+        .body("id", equalTo(udprovider.getId()))
+        .body("label", equalTo(udprovider.getLabel()));
 
-    given()
-        .header("X-Okapi-Tenant", TENANT)
+
+    // GET
+    given().header("X-Okapi-Tenant", TENANT)
         .header("content-type", APPLICATION_JSON)
         .header("accept", APPLICATION_JSON)
         .when()
-        .get(BASE_URI + "/" + usageDataProvider.getId())
+        .get(BASE_URI + "/" + udprovider.getId())
         .then()
         .contentType(ContentType.JSON)
         .statusCode(200)
-        .body("id", equalTo(usageDataProvider.getId()))
-        .body("label", equalTo(usageDataProvider.getLabel()));
+        .body("id", equalTo(udprovider.getId()))
+        .body("label", equalTo(udprovider.getLabel()));
 
-    given()
-        .body("{\n"
-            + " \"id\": \"fc20602a-8ade-4b66-90d0-2853c99affa5\",\n"
-            + " \"label\": \"Nature CHANGED\",\n"
-            + " \"vendorId\": \"uuid-123456789\",\n"
-            + " \"platformId\": \"uuid-123456789\",\n"
-            + " \"harvestingStatus\": \"active\",\n"
-            + " \"serviceType\": \"Sushi Lite\",\n"
-            + " \"serviceUrl\": \"http://example.com\",\n"
-            + " \"reportRelease\": 4,\n"
-            + " \"requestedReports\": [\n"
-            + "  \"JR1\"\n"
-            + " ],"
-            + " \"customerId\": \"12345def\",\n"
-            + " \"requestorId\": \"1234abcd\",\n"
-            + " \"apiKey\": \"678iuoi\",\n"
-            + " \"requestorName\": \"Karla Kolumna\",\n"
-            + " \"requestorMail\": \"CHANGED@ub.uni-leipzig.de\"\n"
-            + "}")
+    // PUT
+    given().body(Json.encode(udproviderChanged))
         .header("X-Okapi-Tenant", TENANT)
         .header("content-type", APPLICATION_JSON)
         .header("accept", "text/plain")
         .request()
-        .put(BASE_URI + "/" + usageDataProvider.getId())
+        .put(BASE_URI + "/" + udproviderChanged.getId())
         .then()
         .statusCode(204);
 
-    UsageDataProvider changed = given()
-        .header("X-Okapi-Tenant", TENANT)
+    // GET again
+    given().header("X-Okapi-Tenant", TENANT)
         .header("content-type", APPLICATION_JSON)
         .header("accept", APPLICATION_JSON)
         .request()
-        .get(BASE_URI + "/" + usageDataProvider.getId())
-        .thenReturn()
-        .as(UsageDataProvider.class);
-    assertThat(changed.getId()).isEqualTo(usageDataProvider.getId());
-    assertThat(changed.getLabel()).isEqualTo("Nature CHANGED");
-    assertThat(changed.getRequestorMail()).isEqualTo("CHANGED@ub.uni-leipzig.de");
+        .get(BASE_URI + "/" + udproviderChanged.getId())
+        .then()
+        .statusCode(200)
+        .body("id", equalTo(udproviderChanged.getId()))
+        .body("label", equalTo("CHANGED"))
+        .body("requestorMail", equalTo("CHANGED@ub.uni-leipzig.de"));
 
-    given()
-        .header("X-Okapi-Tenant", TENANT)
+    // DELETE
+    given().header("X-Okapi-Tenant", TENANT)
         .header("content-type", APPLICATION_JSON)
         .header("accept", "text/plain")
         .when()
-        .delete(BASE_URI + "/" + usageDataProvider.getId())
+        .delete(BASE_URI + "/" + udproviderChanged.getId())
         .then()
         .statusCode(204);
 
-    given()
-        .header("X-Okapi-Tenant", TENANT)
+    // GET again
+    given().header("X-Okapi-Tenant", TENANT)
         .header("content-type", APPLICATION_JSON)
         .header("accept", APPLICATION_JSON)
         .when()
-        .get(BASE_URI + "/" + usageDataProvider.getId())
+        .get(BASE_URI + "/" + udproviderChanged.getId())
         .then()
         .statusCode(404);
   }
@@ -189,24 +171,15 @@ public class UsageDataProvidersIT {
   @Test
   public void checkThatWeCanSearchByCQL() throws UnsupportedEncodingException {
     UsageDataProvider usageDataProvider = given()
-        .body("{\n"
-            + " \"id\": \"b4a196da-434c-475f-9c65-63f9951d1909\",\n"
-            + " \"label\": \"Test Usage Data Provider\",\n"
-            + " \"vendorId\": \"uuid-123456789\",\n"
-            + " \"platformId\": \"uuid-123456789\",\n"
-            + " \"harvestingStatus\": \"active\",\n"
-            + " \"serviceType\": \"Sushi Lite\",\n"
-            + " \"serviceUrl\": \"http://example.com\",\n"
-            + " \"reportRelease\": 4,\n"
-            + " \"requestedReports\": [\n"
-            + "  \"JR1\"\n"
-            + " ],"
-            + " \"customerId\": \"12345def\",\n"
-            + " \"requestorId\": \"1234abcd\",\n"
-            + " \"apiKey\": \"678iuoi\",\n"
-            + " \"requestorName\": \"Karla Kolumna\",\n"
-            + " \"requestorMail\": \"kolumna@ub.uni-leipzig.de\"\n"
-            + "}")
+        .body("{\n" + " \"id\": \"b4a196da-434c-475f-9c65-63f9951d1909\",\n"
+            + " \"label\": \"Test Usage Data Provider\",\n" + " \"vendorId\": \"uuid-123456789\",\n"
+            + " \"platformId\": \"uuid-123456789\",\n" + "\"harvestingStart\": \"2018-01\","
+            + " \"harvestingStatus\": \"active\",\n" + " \"serviceType\": \"Sushi Lite\",\n"
+            + " \"serviceUrl\": \"http://example.com\",\n" + " \"reportRelease\": 4,\n"
+            + " \"requestedReports\": [\n" + "  \"JR1\"\n" + " ],"
+            + " \"customerId\": \"12345def\",\n" + " \"requestorId\": \"1234abcd\",\n"
+            + " \"apiKey\": \"678iuoi\",\n" + " \"requestorName\": \"Karla Kolumna\",\n"
+            + " \"requestorMail\": \"kolumna@ub.uni-leipzig.de\"\n" + "}")
         .header("X-Okapi-Tenant", TENANT)
         .header("content-type", APPLICATION_JSON)
         .header("accept", APPLICATION_JSON)
@@ -218,8 +191,7 @@ public class UsageDataProvidersIT {
     assertThat(usageDataProvider.getId()).isNotEmpty();
 
     String cqlLabel = "?query=(label=\"Test Usage*\")";
-    UdProvidersDataCollection queryResult = given()
-        .header("X-Okapi-Tenant", TENANT)
+    UdProvidersDataCollection queryResult = given().header("X-Okapi-Tenant", TENANT)
         .header("content-type", APPLICATION_JSON)
         .header("accept", APPLICATION_JSON)
         .when()
@@ -236,23 +208,14 @@ public class UsageDataProvidersIT {
   @Test
   public void checkThatInvalidUsageDataProviderIsNotPosted() {
     given()
-        .body("{\n"
-            + " \"id\": \"fc20602a-8ade-4b66-90d0-2853c99affa5\",\n"
-            + " \"vendorId\": \"uuid-123456789\",\n"
-            + " \"platformId\": \"uuid-123456789\",\n"
-            + " \"harvestingStatus\": \"active\",\n"
-            + " \"serviceType\": \"Sushi Lite\",\n"
-            + " \"serviceUrl\": \"http://example.com\",\n"
-            + " \"reportRelease\": 4,\n"
-            + " \"requestedReports\": [\n"
-            + "  \"JR1\"\n"
-            + " ],"
-            + " \"customerId\": \"12345def\",\n"
-            + " \"requestorId\": \"1234abcd\",\n"
-            + " \"apiKey\": \"678iuoi\",\n"
-            + " \"requestorName\": \"Karla Kolumna\",\n"
-            + " \"requestorMail\": \"kolumna@ub.uni-leipzig.de\"\n"
-            + "}")
+        .body("{\n" + " \"id\": \"fc20602a-8ade-4b66-90d0-2853c99affa5\",\n"
+            + " \"vendorId\": \"uuid-123456789\",\n" + " \"platformId\": \"uuid-123456789\",\n"
+            + " \"harvestingStatus\": \"active\",\n" + " \"serviceType\": \"Sushi Lite\",\n"
+            + " \"serviceUrl\": \"http://example.com\",\n" + " \"reportRelease\": 4,\n"
+            + " \"requestedReports\": [\n" + "  \"JR1\"\n" + " ],"
+            + " \"customerId\": \"12345def\",\n" + " \"requestorId\": \"1234abcd\",\n"
+            + " \"apiKey\": \"678iuoi\",\n" + " \"requestorName\": \"Karla Kolumna\",\n"
+            + " \"requestorMail\": \"kolumna@ub.uni-leipzig.de\"\n" + "}")
         .header("X-Okapi-Tenant", TENANT)
         .header("content-type", APPLICATION_JSON)
         .header("accept", APPLICATION_JSON)
