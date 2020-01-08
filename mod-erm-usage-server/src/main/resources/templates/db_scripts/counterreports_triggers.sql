a568858e1979cb03c54d779a13387ebf852e6014
Richard Redweik <redweik@ub.uni-leipzig.de>
2020-01-08 17:12:48 +0100

    Add trigger to write report's error code to udp

--- mod-erm-usage-server/src/main/resources/templates/db_scripts/counterreports_triggers.sql
+++ mod-erm-usage-server/src/main/resources/templates/db_scripts/counterreports_triggers.sql
@@ -57,6 +57,32 @@ END;
 $$
 LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION udp_report_errors(
+  providerId TEXT
+  ) RETURNS jsonb AS
+$$
+DECLARE res jsonb;
+BEGIN
+  SELECT json_agg(error)::jsonb into res
+  FROM (
+    SELECT
+      CASE  WHEN error_code IS NOT NULL THEN error_code
+            ELSE 'other'
+      END AS error
+    FROM(
+      SELECT	DISTINCT(SUBSTRING(jsonb->>'failedReason','Number=([0-9]{4})')) as error_code,
+              COUNT(jsonb->>'failedReason') as number_failed
+      FROM  	diku_mod_erm_usage.counter_reports
+      -- WHERE		jsonb->>'providerId'=$1 AND jsonb->>'failedReason' != ''
+      WHERE		jsonb->>'providerId'=$1 AND jsonb->>'failedReason' IS NOT NULL
+      GROUP BY 	jsonb->>'providerId', error_code, jsonb->>'failedReason')
+    as sub)
+  as sub2;
+  RETURN res;
+END;
+$$
+LANGUAGE plpgsql;
+
 -- trigger function to update latest report available of an usage data provider, on update/insert
 CREATE OR REPLACE FUNCTION update_latest_statistic_on_update() RETURNS TRIGGER AS
 $BODY$
@@ -107,6 +133,30 @@ BEGIN
 END;
 $BODY$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION update_udp_error_codes_on_update() RETURNS TRIGGER AS
+$BODY$
+DECLARE error_codes jsonb;
+BEGIN
+	SELECT udp_report_errors(NEW.jsonb->>'providerId') INTO error_codes;
+	IF error_codes IS NOT NULL THEN
+      UPDATE usage_data_providers SET jsonb = jsonb_set(jsonb, '{reportErrorCodes}', error_codes, TRUE) WHERE jsonb->>'id'=NEW.jsonb->>'providerId';
+  END IF;
+	RETURN NEW;
+END;
+$BODY$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION update_udp_error_codes_on_delete() RETURNS TRIGGER AS
+$BODY$
+DECLARE error_codes jsonb;
+BEGIN
+	SELECT udp_report_errors(OLD.jsonb->>'providerId') INTO error_codes;
+	IF error_codes IS NOT NULL THEN
+    UPDATE usage_data_providers SET jsonb = jsonb_set(jsonb, '{reportErrorCodes}', error_codes, TRUE) WHERE jsonb->>'id'=OLD.jsonb->>'providerId';
+  END IF;
+	RETURN OLD;
+END;
+$BODY$ LANGUAGE plpgsql;
+
 -- trigger to update latest report available of an usage data provider, on update/insert
 DROP TRIGGER IF EXISTS update_provider_report_date_on_update ON counter_reports;
 
@@ -138,3 +188,18 @@ CREATE TRIGGER update_provider_failed_report_on_delete
 AFTER DELETE ON counter_reports
 FOR EACH ROW
 EXECUTE PROCEDURE update_udp_failed_report_on_delete();
+
+DROP TRIGGER IF EXISTS update_provider_report_error_codes_on_update ON counter_reports;
+
+CREATE TRIGGER update_provider_report_error_codes_on_update
+AFTER INSERT OR UPDATE ON counter_reports
+FOR EACH ROW
+EXECUTE PROCEDURE update_udp_error_codes_on_update();
+
+-- trigger to update if an usage data provider has a failed report, on delete
+DROP TRIGGER IF EXISTS update_provider_report_error_codes_on_delete ON counter_reports;
+
+CREATE TRIGGER update_provider_report_error_codes_on_delete
+AFTER DELETE ON counter_reports
+FOR EACH ROW
+EXECUTE PROCEDURE update_udp_error_codes_on_delete();
