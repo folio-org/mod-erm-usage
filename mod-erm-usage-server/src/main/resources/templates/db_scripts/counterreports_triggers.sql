543a6b4d02205b8cd1bb4751007fc617f0332eac
Tino Ryll <ryll@ub.uni-leipzig.de>
2020-08-03 10:05:38 +0200

    Use one trigger function for insert/update/delete Obtain a lock for transaction

--- mod-erm-usage-server/src/main/resources/templates/db_scripts/counterreports_triggers.sql
+++ mod-erm-usage-server/src/main/resources/templates/db_scripts/counterreports_triggers.sql
@@ -56,31 +56,24 @@ END;
 $$
 LANGUAGE plpgsql;
 
--- trigger function to update latest report available of an usage data provider, on update/insert
+-- trigger function to update latest report available of an usage data provider
 CREATE OR REPLACE FUNCTION update_latest_statistic_on_update() RETURNS TRIGGER AS
 $BODY$
 DECLARE latest TEXT;
 DECLARE earliest TEXT;
+DECLARE providerId TEXT;
 BEGIN
-	SELECT latest_year_month(NEW.jsonb->>'providerId') INTO latest;
-	SELECT earliest_year_month(NEW.jsonb->>'providerId') INTO earliest;
-	UPDATE usage_data_providers SET jsonb = jsonb_set(jsonb, '{latestReport}', to_jsonb(latest), TRUE) WHERE jsonb->>'id'=NEW.jsonb->>'providerId';
-	UPDATE usage_data_providers SET jsonb = jsonb_set(jsonb, '{earliestReport}', to_jsonb(earliest), TRUE) WHERE jsonb->>'id'=NEW.jsonb->>'providerId';
-	RETURN NEW;
-END;
-$BODY$ LANGUAGE plpgsql;
-
--- trigger function to update latest report available of an usage data provider, on delete
-CREATE OR REPLACE FUNCTION update_latest_statistic_on_delete() RETURNS TRIGGER AS
-$BODY$
-DECLARE latest TEXT;
-DECLARE earliest TEXT;
-BEGIN
-	SELECT latest_year_month(OLD.jsonb->>'providerId') INTO latest;
-	SELECT earliest_year_month(OLD.jsonb->>'providerId') INTO earliest;
-	UPDATE usage_data_providers SET jsonb = jsonb_set(jsonb, '{latestReport}', to_jsonb(latest), TRUE) WHERE jsonb->>'id'=OLD.jsonb->>'providerId';
-	UPDATE usage_data_providers SET jsonb = jsonb_set(jsonb, '{earliestReport}', to_jsonb(earliest), TRUE) WHERE jsonb->>'id'=OLD.jsonb->>'providerId';
-	RETURN OLD;
+  IF (TG_OP = 'DELETE') THEN
+    providerId := OLD.jsonb->>'providerId';
+  ELSE
+    providerId := NEW.jsonb->>'providerId';
+  END IF;
+  PERFORM pg_advisory_xact_lock(hashtext(providerId));
+	SELECT latest_year_month(providerId) INTO latest;
+	SELECT earliest_year_month(providerId) INTO earliest;
+	UPDATE usage_data_providers SET jsonb = jsonb_set(jsonb, '{latestReport}', to_jsonb(latest), TRUE) WHERE jsonb->>'id' = providerId;
+	UPDATE usage_data_providers SET jsonb = jsonb_set(jsonb, '{earliestReport}', to_jsonb(earliest), TRUE) WHERE jsonb->>'id' = providerId;
+	RETURN NULL;
 END;
 $BODY$ LANGUAGE plpgsql;
 
@@ -126,17 +119,9 @@ $BODY$ LANGUAGE plpgsql;
 DROP TRIGGER IF EXISTS update_provider_report_date_on_update ON counter_reports;
 
 CREATE TRIGGER update_provider_report_date_on_update
-AFTER INSERT OR UPDATE ON counter_reports
+AFTER INSERT OR UPDATE OR DELETE ON counter_reports
 FOR EACH ROW
 EXECUTE PROCEDURE update_latest_statistic_on_update();
-
--- trigger to update latest report available of an usage data provider, on delete
-DROP TRIGGER IF EXISTS update_provider_report_date_on_delete ON counter_reports;
-
-CREATE TRIGGER update_provider_report_date_on_delete
-AFTER DELETE ON counter_reports
-FOR EACH ROW
-EXECUTE PROCEDURE update_latest_statistic_on_delete();
 
 ---- trigger to update reportErrorCodes and hasFailed if an usage data provider has a failed report, on update/insert
 DROP TRIGGER IF EXISTS update_provider_report_error_codes_on_update ON counter_reports;
