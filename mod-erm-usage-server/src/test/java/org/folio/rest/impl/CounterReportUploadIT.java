85abbf7a1a1f4e3d05a8e2149e1f2fc5e25e4d6f
alb3rtino <alb3rtino@users.noreply.github.com>
2020-08-26 14:24:20 +0200

    Ignore another fractional

--- mod-erm-usage-server/src/test/java/org/folio/rest/impl/CounterReportUploadIT.java
+++ mod-erm-usage-server/src/test/java/org/folio/rest/impl/CounterReportUploadIT.java
@@ -307,7 +307,7 @@ public class CounterReportUploadIT {
     Report reportFromDB = Counter4Utils.fromJSON(Json.encode(savedReport.getReport()));
     assertThat(reportFromXML)
         .usingRecursiveComparison()
-        .ignoringFields("created.fractionalSecond")
+        .ignoringFields("created.fractionalSecond", "created.orig_fracSeconds")
         .isEqualTo(reportFromDB);
   }
 
