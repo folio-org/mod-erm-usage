ea2d5b6f55b8426c5118372e861f76970f1d8659
alb3rtino <alb3rtino@users.noreply.github.com>
2020-12-02 19:04:49 +0100

    Reformat CustomReportAPI

--- mod-erm-usage-server/src/main/java/org/folio/rest/impl/CustomReportAPI.java
+++ mod-erm-usage-server/src/main/java/org/folio/rest/impl/CustomReportAPI.java
@@ -17,45 +17,97 @@ public class CustomReportAPI implements CustomReports {
 
   @Override
   @Validate
-  public void getCustomReports(String query, String orderBy,
-      CustomReportsGetOrder order, int offset, int limit, String lang,
-      Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler,
+  public void getCustomReports(
+      String query,
+      String orderBy,
+      CustomReportsGetOrder order,
+      int offset,
+      int limit,
+      String lang,
+      Map<String, String> okapiHeaders,
+      Handler<AsyncResult<Response>> asyncResultHandler,
       Context vertxContext) {
-    PgUtil.get(TABLE_NAME_CUSTOM_REPORTS, CustomReport.class,
-        org.folio.rest.jaxrs.model.CustomReports.class, query, offset, limit, okapiHeaders,
-        vertxContext, GetCustomReportsResponse.class, asyncResultHandler);
+    PgUtil.get(
+        TABLE_NAME_CUSTOM_REPORTS,
+        CustomReport.class,
+        org.folio.rest.jaxrs.model.CustomReports.class,
+        query,
+        offset,
+        limit,
+        okapiHeaders,
+        vertxContext,
+        GetCustomReportsResponse.class,
+        asyncResultHandler);
   }
 
   @Override
   @Validate
-  public void postCustomReports(String lang, CustomReport entity, Map<String, String> okapiHeaders,
-      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    PgUtil.post(TABLE_NAME_CUSTOM_REPORTS, entity, okapiHeaders, vertxContext,
-        PostCustomReportsResponse.class, asyncResultHandler);
-  }
-
-  @Override
-  @Validate
-  public void getCustomReportsById(String id, String lang, Map<String, String> okapiHeaders,
-      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    PgUtil.getById(TABLE_NAME_CUSTOM_REPORTS, CustomReport.class, id, okapiHeaders, vertxContext,
-        GetCustomReportsByIdResponse.class, asyncResultHandler);
-  }
-
-  @Override
-  @Validate
-  public void deleteCustomReportsById(String id, String lang, Map<String, String> okapiHeaders,
-      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    PgUtil.deleteById(TABLE_NAME_CUSTOM_REPORTS, id, okapiHeaders, vertxContext,
-        DeleteCustomReportsByIdResponse.class, asyncResultHandler);
-  }
-
-  @Override
-  @Validate
-  public void putCustomReportsById(String id, String lang, CustomReport entity,
-      Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler,
+  public void postCustomReports(
+      String lang,
+      CustomReport entity,
+      Map<String, String> okapiHeaders,
+      Handler<AsyncResult<Response>> asyncResultHandler,
       Context vertxContext) {
-    PgUtil.put(TABLE_NAME_CUSTOM_REPORTS, entity, id, okapiHeaders, vertxContext,
-        PutCustomReportsByIdResponse.class, asyncResultHandler);
+    PgUtil.post(
+        TABLE_NAME_CUSTOM_REPORTS,
+        entity,
+        okapiHeaders,
+        vertxContext,
+        PostCustomReportsResponse.class,
+        asyncResultHandler);
+  }
+
+  @Override
+  @Validate
+  public void getCustomReportsById(
+      String id,
+      String lang,
+      Map<String, String> okapiHeaders,
+      Handler<AsyncResult<Response>> asyncResultHandler,
+      Context vertxContext) {
+    PgUtil.getById(
+        TABLE_NAME_CUSTOM_REPORTS,
+        CustomReport.class,
+        id,
+        okapiHeaders,
+        vertxContext,
+        GetCustomReportsByIdResponse.class,
+        asyncResultHandler);
+  }
+
+  @Override
+  @Validate
+  public void deleteCustomReportsById(
+      String id,
+      String lang,
+      Map<String, String> okapiHeaders,
+      Handler<AsyncResult<Response>> asyncResultHandler,
+      Context vertxContext) {
+    PgUtil.deleteById(
+        TABLE_NAME_CUSTOM_REPORTS,
+        id,
+        okapiHeaders,
+        vertxContext,
+        DeleteCustomReportsByIdResponse.class,
+        asyncResultHandler);
+  }
+
+  @Override
+  @Validate
+  public void putCustomReportsById(
+      String id,
+      String lang,
+      CustomReport entity,
+      Map<String, String> okapiHeaders,
+      Handler<AsyncResult<Response>> asyncResultHandler,
+      Context vertxContext) {
+    PgUtil.put(
+        TABLE_NAME_CUSTOM_REPORTS,
+        entity,
+        id,
+        okapiHeaders,
+        vertxContext,
+        PutCustomReportsByIdResponse.class,
+        asyncResultHandler);
   }
 }
