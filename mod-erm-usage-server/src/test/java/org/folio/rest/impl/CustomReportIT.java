72e4c51bc03d79a5fc81d1b0b4421992ef7545b7
alb3rtino <alb3rtino@users.noreply.github.com>
2020-12-02 18:42:37 +0100

    Update test and examples

--- mod-erm-usage-server/src/test/java/org/folio/rest/impl/CustomReportIT.java
+++ mod-erm-usage-server/src/test/java/org/folio/rest/impl/CustomReportIT.java
@@ -1,23 +1,27 @@
 package org.folio.rest.impl;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.parsing.Parser;
+import io.restassured.specification.RequestSpecification;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
-import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.Timeout;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
+import java.util.List;
 import java.util.UUID;
+import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration;
 import org.folio.rest.RestVerticle;
 import org.folio.rest.client.TenantClient;
 import org.folio.rest.jaxrs.model.CustomReport;
+import org.folio.rest.jaxrs.model.CustomReports;
 import org.folio.rest.jaxrs.model.TenantAttributes;
 import org.folio.rest.persist.PostgresClient;
 import org.folio.rest.tools.utils.NetworkUtils;
@@ -39,30 +43,49 @@ public class CustomReportIT {
   private static CustomReport reportSecond;
   private static CustomReport reportChanged;
 
-  @Rule
-  public Timeout timeout = Timeout.seconds(10);
+  private final RequestSpecification defaultHeaders =
+      new RequestSpecBuilder()
+          .addHeader("X-Okapi-Tenant", TENANT)
+          .addHeader("content-type", APPLICATION_JSON)
+          .addHeader("accept", APPLICATION_JSON)
+          .addHeader("accept", "text/plain")
+          .build();
+
+  private final RecursiveComparisonConfiguration ignoreMetadata =
+      new RecursiveComparisonConfiguration() {
+        {
+          this.ignoreFields("metadata");
+        }
+      };
+
+  @Rule public Timeout timeout = Timeout.seconds(10);
 
   @BeforeClass
   public static void setUp(TestContext context) {
     vertx = Vertx.vertx();
 
-    reportFirst = new CustomReport()
-        .withId(UUID.randomUUID().toString())
-        .withFileId(UUID.randomUUID().toString())
-        .withFileName("filename.txt")
-        .withFileSize(new Double(1024))
-        .withProviderId(UUID.randomUUID().toString())
-        .withYear(2019);
+    reportFirst =
+        new CustomReport()
+            .withId(UUID.randomUUID().toString())
+            .withFileId(UUID.randomUUID().toString())
+            .withFileName("filename.txt")
+            .withFileSize(1024d)
+            .withProviderId(UUID.randomUUID().toString())
+            .withYear(2019)
+            .withLinkUrl("http://localhost/link1");
 
-    reportSecond = new CustomReport()
-        .withId(UUID.randomUUID().toString())
-        .withFileId(UUID.randomUUID().toString())
-        .withFileName("filename2.txt")
-        .withFileSize(new Double(2024))
-        .withProviderId(UUID.randomUUID().toString())
-        .withYear(2020);
+    reportSecond =
+        new CustomReport()
+            .withId(UUID.randomUUID().toString())
+            .withFileId(UUID.randomUUID().toString())
+            .withFileName("filename2.txt")
+            .withFileSize(2024d)
+            .withProviderId(UUID.randomUUID().toString())
+            .withYear(2020)
+            .withLinkUrl("http://localhost/link2");
 
-    reportChanged = reportFirst.withFileName("newFileName.txt");
+    reportChanged =
+        reportFirst.withFileName("newFileName.txt").withLinkUrl("http://localhost/changed");
 
     try {
       PostgresClient.setIsEmbedded(true);
@@ -113,97 +136,90 @@ public class CustomReportIT {
   @Test
   public void checkThatWeCanAddGetPutAndDeleteCustomReports() {
     // POST
-    given()
-        .body(Json.encode(reportFirst))
-        .header("X-Okapi-Tenant", TENANT)
-        .header("content-type", APPLICATION_JSON)
-        .header("accept", APPLICATION_JSON)
-        .post(BASE_URI)
-        .then()
-        .statusCode(201)
-        .body("fileName", equalTo(reportFirst.getFileName()))
-        .body("id", equalTo(reportFirst.getId()));
+    assertThat(
+            given(defaultHeaders)
+                .body(reportFirst)
+                .post(BASE_URI)
+                .then()
+                .statusCode(201)
+                .extract()
+                .as(CustomReport.class))
+        .usingRecursiveComparison(ignoreMetadata)
+        .isEqualTo(reportFirst);
 
     // GET
-    given()
-        .header("X-Okapi-Tenant", TENANT)
-        .header("content-type", APPLICATION_JSON)
-        .header("accept", APPLICATION_JSON)
-        .get(BASE_URI + "/" + reportFirst.getId())
-        .then()
-        .contentType(ContentType.JSON)
-        .statusCode(200)
-        .body("id", equalTo(reportFirst.getId()));
+    assertThat(
+            given(defaultHeaders)
+                .get(BASE_URI + "/" + reportFirst.getId())
+                .then()
+                .contentType(ContentType.JSON)
+                .statusCode(200)
+                .extract()
+                .as(CustomReport.class))
+        .usingRecursiveComparison(ignoreMetadata)
+        .isEqualTo(reportFirst);
 
     // PUT
-    given()
-        .body(Json.encode(reportChanged))
-        .header("X-Okapi-Tenant", TENANT)
-        .header("content-type", APPLICATION_JSON)
-        .header("accept", "text/plain")
+    given(defaultHeaders)
+        .body(reportChanged)
         .put(BASE_URI + "/" + reportFirst.getId())
         .then()
         .statusCode(204);
 
     // GET again
-    given()
-        .header("X-Okapi-Tenant", TENANT)
-        .header("content-type", APPLICATION_JSON)
-        .header("accept", APPLICATION_JSON)
-        .get(BASE_URI + "/" + reportFirst.getId())
-        .then()
-        .statusCode(200)
-        .body("id", equalTo(reportChanged.getId()))
-        .body("fileName", equalTo(reportChanged.getFileName()));
+    assertThat(
+            given(defaultHeaders)
+                .get(BASE_URI + "/" + reportFirst.getId())
+                .then()
+                .statusCode(200)
+                .extract()
+                .as(CustomReport.class))
+        .usingRecursiveComparison(ignoreMetadata)
+        .isEqualTo(reportChanged);
 
     // POST second report
-    given()
-        .body(Json.encode(reportSecond))
-        .header("X-Okapi-Tenant", TENANT)
-        .header("content-type", APPLICATION_JSON)
-        .header("accept", APPLICATION_JSON)
-        .post(BASE_URI)
-        .then()
-        .statusCode(201)
-        .body("fileName", equalTo(reportSecond.getFileName()))
-        .body("id", equalTo(reportSecond.getId()));
+    assertThat(
+            given(defaultHeaders)
+                .body(reportSecond)
+                .post(BASE_URI)
+                .then()
+                .statusCode(201)
+                .extract()
+                .as(CustomReport.class))
+        .usingRecursiveComparison(ignoreMetadata)
+        .isEqualTo(reportSecond);
 
     // GET all
-    given()
-        .header("X-Okapi-Tenant", TENANT)
-        .header("content-type", APPLICATION_JSON)
-        .header("accept", APPLICATION_JSON)
-        .get(BASE_URI)
-        .then()
-        .statusCode(200)
-        .body("customReports.size()", equalTo(2));
+    assertThat(
+            given(defaultHeaders)
+                .get(BASE_URI)
+                .then()
+                .statusCode(200)
+                .extract()
+                .as(CustomReports.class)
+                .getCustomReports())
+        .hasSize(2)
+        .usingRecursiveComparison(ignoreMetadata)
+        .ignoringCollectionOrder()
+        .isEqualTo(List.of(reportChanged, reportSecond));
 
     // DELETE
-    given()
-        .header("X-Okapi-Tenant", TENANT)
-        .header("content-type", APPLICATION_JSON)
-        .header("accept", "text/plain")
-        .delete(BASE_URI + "/" + reportFirst.getId())
-        .then()
-        .statusCode(204);
+    given(defaultHeaders).delete(BASE_URI + "/" + reportFirst.getId()).then().statusCode(204);
 
     // DELETE
-    given()
-        .header("X-Okapi-Tenant", TENANT)
-        .header("content-type", APPLICATION_JSON)
-        .header("accept", "text/plain")
-        .delete(BASE_URI + "/" + reportSecond.getId())
-        .then()
-        .statusCode(204);
+    given(defaultHeaders).delete(BASE_URI + "/" + reportSecond.getId()).then().statusCode(204);
 
     // GET again
-    given()
-        .header("X-Okapi-Tenant", TENANT)
-        .header("content-type", APPLICATION_JSON)
-        .header("accept", APPLICATION_JSON)
-        .get(BASE_URI + "/" + reportFirst.getId())
-        .then()
-        .statusCode(404);
-  }
+    given(defaultHeaders).get(BASE_URI + "/" + reportFirst.getId()).then().statusCode(404);
 
+    // GET all again
+    assertThat(
+            given(defaultHeaders)
+                .get(BASE_URI)
+                .then()
+                .extract()
+                .as(CustomReports.class)
+                .getCustomReports())
+        .isEmpty();
+  }
 }
