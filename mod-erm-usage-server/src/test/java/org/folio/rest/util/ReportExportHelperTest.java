246797231466283686623c675b4426f3483ce3d6
alb3rtino <alb3rtino@users.noreply.github.com>
2024-11-03 22:22:35 +0100

    Update `createDownloadResponseByReportVersion` in `ReportExportHelper`

--- mod-erm-usage-server/src/test/java/org/folio/rest/util/ReportExportHelperTest.java
+++ mod-erm-usage-server/src/test/java/org/folio/rest/util/ReportExportHelperTest.java
@@ -2,9 +2,14 @@ package org.folio.rest.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.folio.rest.util.ReportExportHelper.CREATED_BY_SUFFIX;
+import static org.folio.rest.util.ReportExportHelper.createDownloadResponseByReportVersion;
 import static org.folio.rest.util.ReportExportHelper.replaceCreatedBy;
 
+import javax.ws.rs.core.MediaType;
+import org.folio.rest.jaxrs.model.CounterReport;
+import org.folio.rest.jaxrs.model.Report;
 import org.junit.Test;
+import org.olf.erm.usage.counter41.Counter4Utils;
 
 public class ReportExportHelperTest {
 
@@ -14,8 +19,8 @@ public class ReportExportHelperTest {
         .isEqualTo("Created_By,Provider " + CREATED_BY_SUFFIX);
     assertThat(replaceCreatedBy("\n\nCreated_By,Provider\n"))
         .isEqualTo("\n\nCreated_By,Provider " + CREATED_BY_SUFFIX + "\n");
-    assertThat(replaceCreatedBy("Created_By,Provider " + CREATED_BY_SUFFIX + ""))
-        .isEqualTo("Created_By,Provider " + CREATED_BY_SUFFIX + "");
+    assertThat(replaceCreatedBy("Created_By,Provider " + CREATED_BY_SUFFIX))
+        .isEqualTo("Created_By,Provider " + CREATED_BY_SUFFIX);
     assertThat(replaceCreatedBy("Created_By,\"Provider, named abc\""))
         .isEqualTo("Created_By,\"Provider, named abc " + CREATED_BY_SUFFIX + "\"");
     assertThat(replaceCreatedBy("Created_By,\"Provider, named abc " + CREATED_BY_SUFFIX + "\""))
@@ -29,5 +34,36 @@ public class ReportExportHelperTest {
     assertThat(replaceCreatedBy("Created_By,")).isEqualTo("Created_By, " + CREATED_BY_SUFFIX);
     assertThat(replaceCreatedBy("Created_by,")).isEqualTo("Created_by,");
     assertThat(replaceCreatedBy(null)).isNull();
+  }
+
+  @Test
+  public void testCreateDownloadResponseByReportVersion() {
+    assertThat(
+            createDownloadResponseByReportVersion(
+                new CounterReport().withRelease("4").withReport(new Report())))
+        .satisfies(
+            response -> {
+              assertThat(response.getMediaType()).isEqualTo(MediaType.APPLICATION_XML_TYPE);
+              assertThat(response.getEntity()).isEqualTo(Counter4Utils.toXML("{}"));
+            });
+
+    assertThat(
+            createDownloadResponseByReportVersion(
+                new CounterReport().withRelease("4").withReport(null)))
+        .isNull();
+
+    assertThat(
+            createDownloadResponseByReportVersion(
+                new CounterReport().withRelease("5").withReport(new Report())))
+        .satisfies(
+            response -> {
+              assertThat(response.getMediaType()).isEqualTo(MediaType.APPLICATION_JSON_TYPE);
+              assertThat(response.getEntity()).isEqualTo("{}");
+            });
+
+    assertThat(
+            createDownloadResponseByReportVersion(
+                new CounterReport().withRelease("5").withReport(null)))
+        .isNull();
   }
 }
