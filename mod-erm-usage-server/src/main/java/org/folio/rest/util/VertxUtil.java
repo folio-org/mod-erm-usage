0e8b73bf1b5a2bef0458820308a1c480c3667d62
alb3rtino <alb3rtino@users.noreply.github.com>
2026-01-07 16:32:33 +0100

    Adjust VertxUtil for Vert.x 5

--- mod-erm-usage-server/src/main/java/org/folio/rest/util/VertxUtil.java
+++ mod-erm-usage-server/src/main/java/org/folio/rest/util/VertxUtil.java
@@ -2,16 +2,14 @@ package org.folio.rest.util;
 
 import io.vertx.core.Context;
 import io.vertx.core.Future;
-import io.vertx.core.Promise;
 import java.util.function.Supplier;
 
 public class VertxUtil {
 
   private VertxUtil() {}
 
+  // TODO: useless now, refactor usages to use vertxContext directly
   public static <T> Future<T> executeBlocking(Context vertxContext, Supplier<T> supplier) {
-    Promise<T> result = Promise.promise();
-    vertxContext.executeBlocking(promise -> promise.complete(supplier.get()), result);
-    return result.future();
+    return vertxContext.executeBlocking(supplier::get);
   }
 }
